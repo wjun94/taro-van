374c810b417596c9116cf07a80f77a16c69ce579
项文俊 <xiangwenjun1@zhenhuipin.cn>
2022-02-11 13:02:51 +0800

    feat: 调整package.json

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,12 +5,12 @@ import './index.less';
 
 const Index = () => {
   return (
-    <View className="wrapper">
-      <Button type="primary">主要按钮</Button>
-      <Button type="info">信息按钮</Button>
-      <Button type="danger">危险按钮</Button>
-      <Button type="default">默认按钮</Button>
-      <Button type="warning">警告按钮</Button>
+    <View className='wrapper'>
+      <Button type='primary'>主要按钮</Button>
+      <Button type='info'>信息按钮</Button>
+      <Button type='danger'>危险按钮</Button>
+      <Button type='default'>默认按钮</Button>
+      <Button type='warning'>警告按钮</Button>
     </View>
   );
 };
