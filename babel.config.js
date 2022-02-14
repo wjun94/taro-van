9ee3efc03c79b63e61810672481ad8a4821d5119
项文俊 <wj.xiang>
2022-02-14 23:07:26 +0800

    feat: 解决umi编译报错问题

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,27 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
-module.exports = {
-  presets: [
-    [
-      'taro',
-      {
-        framework: 'react',
-        ts: true,
-      },
-    ],
-  ],
-  plugins: [
-    // ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
-    [
-      'import',
-      {
-        libraryName: 'taro-hooks',
-        camel2DashComponentName: false,
-      },
-      'taro-hooks',
-    ],
-  ],
-};
+module.exports =
+  process.env.APP_ROOT === 'dumi'
+    ? {}
+    : {
+        presets: [
+          [
+            'taro',
+            {
+              framework: 'react',
+              ts: true,
+            },
+          ],
+        ],
+        plugins: [
+          // ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
+          [
+            'import',
+            {
+              libraryName: 'taro-hooks',
+              camel2DashComponentName: false,
+            },
+            'taro-hooks',
+          ],
+        ],
+      };
