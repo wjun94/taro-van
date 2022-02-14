c3234c6095cc0e1d23fdc311775925fb8cd57bd2
项文俊 <wj.xiang>
2022-02-14 23:12:05 +0800

    feat: 调整编译h5路径，打包文档添加umi和taro h5文件

--- babel.config.js
+++ babel.config.js
@@ -14,7 +14,6 @@ module.exports =
           ],
         ],
         plugins: [
-          // ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
           [
             'import',
             {
