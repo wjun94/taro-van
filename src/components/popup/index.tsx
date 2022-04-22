36f2499c64adb8cfc490e3b8872f0d21dc5f61f9
项文俊 <xiangwenjun1@zhenhuipin.cn>
2022-04-22 15:52:55 +0800

    feat: 添加Popup弹出层组件

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -2,13 +2,19 @@ import { View } from '@tarojs/components';
 import { ViewProps } from '@tarojs/components/types/View';
 import classNames from 'classnames';
 import { ReactNode } from 'react';
+import Flex from '../flex';
+import Icon from '../icon';
 import Overlay from '../overlay';
+import Typography from '../typography';
 
 export type P = {
   children?: ReactNode;
   visible?: boolean;
   onClose?: () => void;
   closeOnMaskClick?: boolean;
+  round?: boolean; // 圆角
+  title?: string;
+  closeIcon?: boolean;
   position?: 'top' | 'left' | 'right' | 'bottom';
 };
 
@@ -18,6 +24,9 @@ const TvPopup = ({
   style,
   closeOnMaskClick = true,
   position = 'bottom',
+  round,
+  closeIcon,
+  title,
   onClose,
   ...props
 }: P & ViewProps) => {
@@ -26,6 +35,7 @@ const TvPopup = ({
     prefixCls,
     {
       [`${prefixCls}--${position}`]: position,
+      [`${prefixCls}--round`]: round,
       [`${prefixCls}--${position}__open`]: props.visible,
       [`${prefixCls}--${position}__close`]: !props.visible,
     },
@@ -37,8 +47,26 @@ const TvPopup = ({
       animate
       {...props}
     >
-      <View style={style} className={classes}>
-        {children}
+      <View
+        style={style}
+        onClick={(e) => e.stopPropagation()}
+        className={classes}
+      >
+        <View className={`${prefixCls}--body`}>
+          {title && (
+            <Flex
+              align='center'
+              justify='center'
+              className={`${prefixCls}--head`}
+            >
+              {title && <Typography.Title level={2}>{title}</Typography.Title>}
+            </Flex>
+          )}
+          {closeIcon && (
+            <Icon onClick={() => onClose && onClose()} icon='icon-cross' />
+          )}
+          {children}
+        </View>
       </View>
     </Overlay>
   );
