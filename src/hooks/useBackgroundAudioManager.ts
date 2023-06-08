9261ef1fc86cf3a051ad746d507c46661416a3ef
wjun <994037715@qq.com>
2023-06-08 15:16:00 +0800

    fix: 删除无用代码

--- src/hooks/useBackgroundAudioManager.ts
+++ src/hooks/useBackgroundAudioManager.ts
@@ -23,6 +23,7 @@ export const useBackgroundAudioManager = () => {
     });
     manager.current.onPlay(() => setPlaying(true));
     manager.current.onPause(() => setPlaying(false));
+    manager.current.onStop(() => setPlaying(false));
     manager.current.onEnded(() => {
       setPlaying(false);
     });
